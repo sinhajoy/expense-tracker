6dabdedd7e34b0e1ff546f64af62202120ebd6dd
JOY SINHA <51963863+sinhajoy@users.noreply.github.com>
2022-04-14 00:05:22 +0530

    main

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,8 +1,16 @@
 import './ExpenseItem.css';
 function ExpenseItem(){
+    const expenseDate=new Date(2022, 2, 1);
+    const expenseTitle="Condom";
+    const expenseAmmount=295;
+
     return(
-        <div className=''>
-            <h2>Expense Item</h2>
+        <div className='expense-item'>
+            <div>{expenseDate.toISOString()}</div>
+            <div className='expense-item__description'>
+                <h2>{expenseTitle}</h2>
+                <div className='expense-item__price'>${expenseAmmount}</div>
+            </div>
         </div>
 
     );
