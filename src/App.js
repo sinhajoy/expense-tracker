466bf4ee7df004b80e62a8cb4691eccf22c7d80c
JOY SINHA <51963863+sinhajoy@users.noreply.github.com>
2022-04-14 11:19:54 +0530

    main

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import './App.css';
 import './components/ExpenseItem';
-import ExpenseItem from './components/ExpenseItem';
+import Expenses from './components/Expenses';
 
 function App() {
+  const expenses=[
+    {
+    title:"Condom",amount:209,date:new Date(2020,2,1),
+  },
+  {
+    title:"Vibrator",amount:201,date:new Date(2020,2,1),
+  },
+  {
+    title:"Lube",amount:300,date:new Date(2020,2,1),
+  },{
+    title:"Tissues",amount:10,date:new Date(2020,2,1),
+  },
+];
   return (
     <div className="App">
-      <h2>My name is joy</h2>
-      <ExpenseItem></ExpenseItem>
+      <h2>Joy's Expense Tracker</h2>
+      <Expenses items={expenses}/>
     </div>
   );
 }
